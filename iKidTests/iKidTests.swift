48e53c11e30eb1138315d9f1cf3fcb556d91c60d
iversonli <iversonli88@gmail.com>
2025-05-01 13:24:24 -0700

    Initial Commit

---
+++ iKidTests/iKidTests.swift
@@ -0,0 +1,17 @@
+//
+//  iKidTests.swift
+//  iKidTests
+//
+//  Created by Iverson Li on 5/1/25.
+//
+
+import Testing
+@testable import iKid
+
+struct iKidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
